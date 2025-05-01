865896c6c7c67edb95ef86f9a28065b7b97ee98a
jak713 <juliakaczmarek00@gmail.com>
2025-05-01 14:02:44 +0100

    introduced new bugs

--- qtaim.py
+++ qtaim.py
@@ -3,7 +3,7 @@ import re
 import math as m
 
 class QTAIM:
-    __version__ = "1.0.3"
+    __version__ = "1.0.4"
 
     def __init__(self, file):
         self.file = file
@@ -14,7 +14,7 @@ class QTAIM:
             "CP_no": [],
             "type": [],
             "connected_atoms": [],
-            "rho": [],
+            "rho": [], 
             "laplacian": [],
             "energy_density": [],
             "potential_energy_density": [],
@@ -96,12 +96,15 @@ class QTAIM:
 #################################################################
 # --- Visualisation Code ---
 #################################################################
-    def visualise(self, xyz_file, show_cp = False, show_rho = False, show_lap = False, show_pos_lap = False, show_bond_lengths = False, aboveXangstrom = False, belowXangstrom =False, X = None, hide_ring_cage = False, show_only_same = False, show_only_different = False, show_atom_labels = False, connect_atoms_A_B = False, A=None, B=None):
+    def visualise(self, xyz_file, show_cp = False, show_rho = False, show_lap = False, show_pos_lap = False, show_bond_lengths = False, aboveXangstrom = False, belowXangstrom =False, X = None, hide_ring_cage = False, show_only_same = False, show_only_different = False, show_atom_labels = False, connect_atoms_A_B = False, A=None, B=None, covalent = True, xyz_outline=False, print_parameters = False, legend = True):
         """Visualise the bond critical points w/ Py3Dmol
             use xyz_file to get coordinates for connecting the CPs
             according to the connected_atoms list"""
+        if print_parameters:
+            headers_printed = False
+            cp_of_interest_num = 0
         self.get_coordinates()
-        view = py3Dmol.view(width=1400, height=1000)
+        view = py3Dmol.view(width=1200, height=1000)
 
 
         with open(xyz_file, 'r') as f:
@@ -117,21 +120,12 @@ class QTAIM:
         
         # find indexes of connected atoms and use that to get the coordinates from the coordinates tuples list (offset index by 1 for python, connected_atoms is 1-indexed)
         for i, atoms in enumerate(self.parameters["connected_atoms"]):
+            if connect_atoms_A_B and A is not None and B is not None:
+                if not (A in self.parameters["connected_atoms"][i] and B in self.parameters["connected_atoms"][i]):
+                    continue
             if show_only_different and self.is_same_atom_type(i):
                 continue
             if show_only_same and not self.is_same_atom_type(i):
-                continue
-            if connect_atoms_A_B and A is not None and B is not None:
-                if A in atoms and B in atoms:
-                    atom_indexs = re.findall(r'\d+', atoms)
-                    if len(atom_indexs) >= 2:
-                        atom1 = int(atom_indexs[0]) - 1
-                        atom2 = int(atom_indexs[1]) - 1
-                        coord1 = coordinates[atom1]
-                        coord2 = coordinates[atom2]
-                        view.addLine({'start': {'x': coord1[0], 'y': coord1[1], 'z': coord1[2]},
-                                'end':   {'x': coord2[0], 'y': coord2[1], 'z': coord2[2]},
-                                'color': "black", 'radius': 5})
                 continue
             if not atoms:
                 continue
@@ -145,6 +139,15 @@ class QTAIM:
                 bcp_z = self.parameters["z_coord"][cp_no]
                 coord1 = coordinates[atom1]
                 coord2 = coordinates[atom2]
+                dist1 = m.sqrt((coord1[0] - bcp_x)**2 + (coord1[1] - bcp_y)**2 + (coord1[2] - bcp_z)**2)
+                dist2 = m.sqrt((coord2[0] - bcp_x)**2 + (coord2[1] - bcp_y)**2 + (coord2[2] - bcp_z)**2)
+                length = dist1 + dist2
+                if aboveXangstrom and X is not None:
+                    if length < X:
+                        continue
+                if belowXangstrom and X is not None:
+                    if length > X:
+                        continue
                 color = "green" if self.is_same_atom_type(cp_no) else "red"
                 view.addLine({'start': {'x': coord1[0], 'y': coord1[1], 'z': coord1[2]},
                         'end':   {'x': bcp_x, 'y': bcp_y, 'z': bcp_z},
@@ -163,53 +166,109 @@ class QTAIM:
                 continue
             if show_only_different and self.is_same_atom_type(i):
                 continue
+            if connect_atoms_A_B and A is not None and B is not None:
+                if not (A in self.parameters["connected_atoms"][i] and B in self.parameters["connected_atoms"][i]):
+                    continue    
             x = self.parameters["x_coord"][i]
             y = self.parameters["y_coord"][i]
             z = self.parameters["z_coord"][i]
-            color = "lightblue" if self.parameters["type"][i] == "(3,-1)" and self.parameters["laplacian"][i] > 0 else "blue" if self.parameters["type"][i] == "(3,-1)" else "red" if self.parameters["type"][i] == "(3,+1)" else "green" if self.parameters["type"][i] == "(3,+3)" else "yellow"
+            connected = self.parameters["connected_atoms"][i]
+            if not connected:
+                continue
+            atom_indexs = re.findall(r'\d+', connected)
+            if len(atom_indexs) >= 2:
+                atom1 = int(atom_indexs[0]) - 1
+                atom2 = int(atom_indexs[1]) - 1
+                cp_no = int(self.parameters["CP_no"][self.parameters["connected_atoms"].index(atoms)]) - 1
+                # bcp_x = self.parameters["x_coord"][cp_no]
+                # bcp_y = self.parameters["y_coord"][cp_no]
+                # bcp_z = self.parameters["z_coord"][cp_no]
+                coord1 = coordinates[atom1]
+                coord2 = coordinates[atom2]
+                dist1 = m.sqrt((coord1[0] - x)**2 + (coord1[1] - y)**2 + (coord1[2] - z)**2)
+                dist2 = m.sqrt((coord2[0] - x)**2 + (coord2[1] - y)**2 + (coord2[2] - z)**2)
+                length = dist1 + dist2
+            if aboveXangstrom and X is not None:
+                if length < X:
+                    continue
+            if belowXangstrom and X is not None:
+                if length > X:
+                    continue
+
+            if not covalent:
+                if self.parameters["type"][i] == "(3,-1)" and self.parameters["laplacian"][i] < 0:
+                    continue
+                elif self.parameters["type"][i] == "(3,-3)":
+                    continue
+                color = "lightblue" if self.parameters["type"][i] == "(3,-1)" and self.parameters["laplacian"][i] > 0  else "red" if self.parameters["type"][i] == "(3,+1)" else "green" if  self.parameters["type"][i] == "(3,+3)" else "green" 
+            else:    
+                color = "lightblue" if self.parameters["type"][i] == "(3,-1)" and self.parameters["laplacian"][i] > 0 else "blue" if self.parameters["type"][i] == "(3,-1)" else "red" if self.parameters["type"][i] == "(3,+1)" else "green" if self.parameters["type"][i] == "(3,+3)" else "yellow"
             view.addSphere({'center': {'x': x, 'y': y, 'z': z}, 'radius': 0.1, 'color': color})
+            
+            if print_parameters:
+                if not headers_printed:
+                    print("{:<5} {:<10} {:<20} {:<10} {:<12} {:<15} {:<20} {:<20} {:<15}".format(
+                        "CP", "Type", "ConnectedAtoms", "Rho", "Laplacian", "EnergyDensity", "PotentialEDensity", "LagrangianKcEnergy", "IntDist"
+                    ))
+                    headers_printed = True 
+                
+                print("{:<5} {:<10} {:<20} {:<10.3f} {:<12.3f} {:<15.3f} {:<20.3f} {:<20.3f} {:<15.2f}".format(
+                    self.parameters["CP_no"][i],
+                    self.parameters["type"][i],
+                    self.parameters["connected_atoms"][i],
+                    self.parameters["rho"][i],
+                    self.parameters["laplacian"][i],
+                    self.parameters["energy_density"][i],
+                    self.parameters["potential_energy_density"][i],
+                    self.parameters["lagrangian_kinetic_energy"][i],
+                    length if length else "N/A"
+                ))
+                cp_of_interest_num += 1
+        if print_parameters:
+            print(f"Total number of interactions of interest: {cp_of_interest_num}")
 
         # legend for the different colors, positioned to avoid overlap
-        legend_items = [
-            {"label": "Bond Critical Point", "color": "blue", "mode": "sphere"},
-            {"label": "Bond Critical Point (positive Laplacian)", "color": "lightblue", "mode": "sphere"},
-            {"label": "Ring Critical Point", "color": "red", "mode": "sphere"},
-            {"label": "Cage Critical Point", "color": "green", "mode": "sphere"},
-            {"label": "Nuclear Critical Point", "color": "yellow", "mode": "sphere"},
-            {"label": "Same Connecting Atoms", "color": "green", "mode": "line"},
-            {"label": "Different Connecting Atoms", "color": "red", "mode": "line"}
-        ]
-        # set starting position and spacing offsets for the legend items
-        start_x = -5
-        start_y = 5
-        spacing = 1.0
-        for i, item in enumerate(legend_items):
-            y_pos = start_y - i * spacing
-            if item["mode"] == "sphere":
-                sphere_pos = {'x': start_x, 'y': y_pos, 'z': 0}
-                label_pos = {'x': start_x + 0.5, 'y': y_pos, 'z': 0}
-                view.addSphere({'center': sphere_pos, 'radius': 0.1, 'color': item["color"]})
-                view.addLabel(item["label"], {
-                    'position': label_pos,
-                    'backgroundColor': 'white',
-                    'backgroundOpacity': 0.5,
-                    'fontSize': 10,
-                    'fontColor': 'black',
-                    'fontWeight': 'bold'
-            })
-            elif item["mode"] == "line":
-                line_start = {'x': start_x, 'y': y_pos, 'z': 0}
-                line_end = {'x': start_x + 0.5, 'y': y_pos, 'z': 0}
-                label_pos = {'x': start_x + 0.6, 'y': y_pos, 'z': 0}
-                view.addLine({'start': line_start, 'end': line_end, 'color': item["color"], 'radius': 5})
-                view.addLabel(item["label"], {
-                    'position': label_pos,
-                    'backgroundColor': 'white',
-                    'backgroundOpacity': 0.5,
-                    'fontSize': 10,
-                    'fontColor': 'black',
-                    'fontWeight': 'bold'
+        if legend:
+            legend_items = [
+                {"label": "Bond Critical Point", "color": "blue", "mode": "sphere"},
+                {"label": "Bond Critical Point (positive Laplacian)", "color": "lightblue", "mode": "sphere"},
+                {"label": "Ring Critical Point", "color": "red", "mode": "sphere"},
+                {"label": "Cage Critical Point", "color": "green", "mode": "sphere"},
+                {"label": "Nuclear Critical Point", "color": "yellow", "mode": "sphere"},
+                {"label": "Same Connecting Atoms", "color": "green", "mode": "line"},
+                {"label": "Different Connecting Atoms", "color": "red", "mode": "line"}
+            ]
+            # set starting position and spacing offsets for the legend items
+            start_x = -5
+            start_y = 5
+            spacing = 1.0
+            for i, item in enumerate(legend_items):
+                y_pos = start_y - i * spacing
+                if item["mode"] == "sphere":
+                    sphere_pos = {'x': start_x, 'y': y_pos, 'z': 0}
+                    label_pos = {'x': start_x + 0.5, 'y': y_pos, 'z': 0}
+                    view.addSphere({'center': sphere_pos, 'radius': 0.1, 'color': item["color"]})
+                    view.addLabel(item["label"], {
+                        'position': label_pos,
+                        'backgroundColor': 'white',
+                        'backgroundOpacity': 0.5,
+                        'fontSize': 10,
+                        'fontColor': 'black',
+                        'fontWeight': 'bold'
                 })
+                elif item["mode"] == "line":
+                    line_start = {'x': start_x, 'y': y_pos, 'z': 0}
+                    line_end = {'x': start_x + 0.5, 'y': y_pos, 'z': 0}
+                    label_pos = {'x': start_x + 0.6, 'y': y_pos, 'z': 0}
+                    view.addLine({'start': line_start, 'end': line_end, 'color': item["color"], 'radius': 5})
+                    view.addLabel(item["label"], {
+                        'position': label_pos,
+                        'backgroundColor': 'white',
+                        'backgroundOpacity': 0.5,
+                        'fontSize': 10,
+                        'fontColor': 'black',
+                        'fontWeight': 'bold'
+                    })
                 
         if show_atom_labels:
             """show atom type labels for the atoms in the xyz file"""
@@ -246,9 +305,25 @@ class QTAIM:
                     continue
                 if show_only_different and self.is_same_atom_type(i):
                     continue
+                if connect_atoms_A_B and A is not None and B is not None:
+                    if A == B:
+                        if A not in self.parameters["connected_atoms"][i]:
+                            continue
+                    else:
+                        if not (A in self.parameters["connected_atoms"][i] and B in self.parameters["connected_atoms"][i]):
+                            continue
                 x = self.parameters["x_coord"][i]
                 y = self.parameters["y_coord"][i]
                 z = self.parameters["z_coord"][i]
+                dist1 = m.sqrt((x - coordinates[0][0])**2 + (y - coordinates[0][1])**2 + (z - coordinates[0][2])**2)
+                dist2 = m.sqrt((x - coordinates[1][0])**2 + (y - coordinates[1][1])**2 + (z - coordinates[1][2])**2)
+                length = dist1 + dist2
+                if aboveXangstrom and X is not None:
+                    if length < X:
+                        continue
+                if belowXangstrom and X is not None:
+                    if length > X:
+                        continue
                 label = f"{cp_no}"
                 sphere_color = "lightblue" if self.parameters["type"][i] == "(3,-1)" and self.parameters["laplacian"][i] > 0 else \
                     "blue" if self.parameters["type"][i] == "(3,-1)" else \
@@ -274,6 +349,9 @@ class QTAIM:
                     continue
                 if show_only_different and self.is_same_atom_type(i):
                     continue
+                if connect_atoms_A_B and A is not None and B is not None:
+                    if not (A in self.parameters["connected_atoms"][i] and B in self.parameters["connected_atoms"][i]):
+                        continue
                 x = self.parameters["x_coord"][i]
                 y = self.parameters["y_coord"][i]
                 z = self.parameters["z_coord"][i]
@@ -303,6 +381,9 @@ class QTAIM:
                     continue
                 if show_only_different and self.is_same_atom_type(i):
                     continue
+                if connect_atoms_A_B and A is not None and B is not None:
+                    if not (A in self.parameters["connected_atoms"][i] and B in self.parameters["connected_atoms"][i]):
+                        continue
                 x = self.parameters["x_coord"][i]
                 y = self.parameters["y_coord"][i]
                 z = self.parameters["z_coord"][i]
@@ -334,6 +415,9 @@ class QTAIM:
                     continue
                 if show_only_different and self.is_same_atom_type(i):
                     continue
+                if connect_atoms_A_B and A is not None and B is not None:
+                    if not (A in self.parameters["connected_atoms"][i] and B in self.parameters["connected_atoms"][i]):
+                        continue
                 x = self.parameters["x_coord"][i]
                 y = self.parameters["y_coord"][i]
                 z = self.parameters["z_coord"][i]
@@ -362,6 +446,9 @@ class QTAIM:
                     continue
                 if show_only_same and not self.is_same_atom_type(i):
                     continue
+                if connect_atoms_A_B and A is not None and B is not None:
+                    if not (A in self.parameters["connected_atoms"][i] and B in self.parameters["connected_atoms"][i]):
+                        continue
                 if not atoms:
                     continue
                 atom_indexs = re.findall(r'\d+', atoms)
@@ -406,9 +493,11 @@ class QTAIM:
                             'fontWeight': 'bold',
                             'backgroundOpacity': 0.0
                         })
+        if xyz_outline:
+            # add xyz molecule structure into the view
+            view.addModel(open(xyz_file, 'r').read(), 'xyz')
+            view.setStyle({'stick': {'radius': 0.03}})
 
-
-        view.setStyle({'stick': {}})
         view.setBackgroundColor('white')
         view.zoomTo()
         # Show the view
